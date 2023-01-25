05cee65a51428267e777a26cdb10f80be7cd1218
ziondayoo <adetunjitemidayo26@gmail.com>
2023-01-25 14:18:25 +0100

    Added Dtos

---
+++ WeatherAPI/WeatherDomain/Dtos/InformationDto.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherAPI.WeatherDomain.Dtos
+{
+    public class InformationDto
+    {
+        public double Temperature { get; set; }
+        public string Details { get; set; }
+        public string Summary { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+        public string Icon { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+
+    }
+}
