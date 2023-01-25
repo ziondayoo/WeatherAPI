705076db12e056235579638cea77a5252506ed16
ziondayoo <adetunjitemidayo26@gmail.com>
2023-01-25 17:19:42 +0100

    Added AuthController and WeatherForecast Controller

--- WeatherAPI/Controllers/AuthController.cs
+++ WeatherAPI/Controllers/AuthController.cs
@@ -1,6 +1,31 @@
-﻿namespace WeatherAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.WeatherDomain.Core.Services;
+using WeatherAPI.WeatherDomain.Dtos;
+
+namespace WeatherAPI.Controllers
 {
-    public class AuthController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserDto request)
+        {
+            return Ok(await _authService.Register(request));    
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserDto request)
+        {
+            return Ok(await _authService.Login(request));
+        }
+
     }
 }
---
+++ WeatherAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.WeatherInfrastructure.WeatherRepository;
+
+namespace WeatherAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private IWeatherRepository _weatherService;
+
+        public WeatherForecastController(IWeatherRepository weatherService)
+        {
+            _weatherService = weatherService;
+        }
+        [HttpGet(Name = "GetWeatherForecast"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Get (string location)
+        {
+            return Ok(await _weatherService.GetCurrentWeatherAsync(location));
+        }
+    }
+}
